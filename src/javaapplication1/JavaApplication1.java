2589cf6bf7e7152383ebdb24fcb3ba35222ebb2f
EvgenyMSH <shanturov.work@yandex.com>
2022-08-09 16:38:42 +0400

    second commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author evgeny
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println ("H   H eeee ");
+        System.out.println ("H   H e   e");
+        System.out.println ("HHHHH eeeee");
+        System.out.println ("H   H e     ");
+        System.out.println ("H   H eeeee ");
+        // TODO code application logic here
+    }
+    
+}
